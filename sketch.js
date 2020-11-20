45a26fdc0033f4931dadbc7656da3808abbc0b8f
kartik-gamemaker <74004513+kartik-gamemaker@users.noreply.github.com>
2020-11-20 16:48:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+const Engine=Matter.Engine;
+const World=Matter.World;
+const Bodies=Matter.Bodies;
+
+var world,engine;
+var ball;
+var ground;
+function setup() {
+  createCanvas(800,400);
+  engine=Engine.create();
+  world=engine.world;
+
+  var options={
+    restitution:1.0
+  }
+
+  ball=Bodies.circle(200,100,60,options);
+  World.add(world,ball);
+  var ground_options={
+    isStatic:true
+  }
+  ground = Bodies.rectangle(200,390,200,20,ground_options);
+  World.add(world,ground)
+}
+
+function draw() {
+  background(0);  
+  Engine.update(engine);
+
+  circle(ball.position.x,ball.position.y,60);
+  rect(ground.position.x,ground.position.y,400,20);
+}
